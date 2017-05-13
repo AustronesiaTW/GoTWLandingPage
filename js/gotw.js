b2b893ac29c02c34c4a56700b8ac9ffda88f9b15
travishen <travishen.tw@gmail.com>
2017-05-13 17:52:47 +0800

    Template initial

---
+++ js/gotw.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	//Pace on done
+	Pace.on('done',function(){
+		$('#mainDiv').fadeIn(500);	
+	});
+
+	i18nHelper.Init();
+	//Get browser language 
+	var userLang = navigator.language || navigator.userLanguage; 
+	var selection = 'en_US';
+	
+	var lang_list = [];
+	
+	$('#dropdown-lang option').each(function(){
+		if($(this).data().lang == userLang.replace('-','_')){
+			$(this).attr('selected',true);	
+			selection = userLang;			
+		}
+	});
+	
+	i18nHelper.LoadBundles(selection);
+
+
+});
+
+var i18nHelper = {
+	Init: function(){
+		$('#dropdown-lang').change(function() {
+			var selection = $('#dropdown-lang option:selected').data().lang;
+			i18nHelper.LoadBundles(selection);
+		});
+	},
+	LoadBundles: function(lang){
+		$.i18n.properties({
+			name:'Messages', 
+			path:'lib/jquery-i18n/bundle/', 
+			mode:'both',
+			language:lang, 
+			callback: function() {
+				i18nHelper.UpdateMessages();
+			}
+		});
+	},
+	UpdateMessages: function(){
+		$('[data-msg]').each(function(){
+        		var msg = $(this).data().msg;
+        		$(this).html(eval(msg));
+      		});	
+	
+	}
+}
+
+
+		
