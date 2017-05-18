e4230e129ca47467199e655f5f881060f58f1935
travishen <travishen.tw@gmail.com>
2017-05-18 14:30:20 +0800

    Update gotw.js

--- js/gotw.js
+++ js/gotw.js
@@ -29,7 +29,7 @@ var i18nHelper = {
 		$('#dropdown-lang').change(function() {
 			var selection = $('#dropdown-lang option:selected').data().lang;
 			i18nHelper.LoadBundles(selection);
-			$('[lang]').attr('lang',userLang)
+			$('[lang]').attr('lang',selection);
 		});
 	},
 	LoadBundles: function(lang){
